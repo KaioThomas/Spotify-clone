776bd75e12da2a373f105a1139fb7f14aa94fb28
KaioThomas <kaiopsouza59@gmail.com>
2022-09-04 15:23:43 -0300

    criando spotify configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,22 @@ export const environment = {
   production: false
 };
 
+export const SpotifyConfiguration = {
+  clientId: 'a5f4ca99abb64492ab2df40cab228226',
+  authEndPoint: 'https://accounts.spotify.com/authorize',
+  redirectUrl: 'http://localhost:4200/login/',
+  scopes: [
+    "user-read-currently-playing", // musica tocando agora.
+    "user-read-recently-played", // ler musicas tocadas recentemente
+    "user-read-playback-state", // ler estado do player do usuário
+    "user-top-read", // top artistas e musicas do usuário
+    "user-modify-playback-state", // alterar do player do usuário.
+    "user-library-read", // ler biblioteca dos usuários
+    "playlist-read-private", // ler playlists privadas
+    "playlist-read-collaborative" // ler playlists colaborativas
+  ]
+}
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
